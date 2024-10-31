a1315011fbc6ca49bea39ffdc77fef5d6bc893e4
Cronvus <Cronvus@gmail.com>
2024-10-31 03:09:18 +0300

    Fix + new js

--- src/js/buttonBodyNext.js
+++ src/js/buttonBodyNext.js
@@ -10,9 +10,20 @@ bodyNext.addEventListener('click', function(evt){
   evt.preventDefault();
   if(document.documentElement.offsetWidth < 662) {
     arrowButtonLabel.textContent = arrowButtonLabel.textContent === "Читать далее" ? "Скрыть" : 'Читать далее';
-    arrowButton.style.transform === "rotate(230deg)" ? arrowButton.style.transform = "rotate(45deg)" : arrowButton.style.transform = "rotate(230deg)";
-    bodyInfoText.style.height === 'fit-content' ? bodyInfoText.style.height = '90px' : bodyInfoText.style.height = 'fit-content';
-    console.log('Клик')
+    arrowButtonLabel.style.color = 'black';
+    for(var i =0; i<arrowButton.length; i++) {
+      arrowButton[i].style.transform === "rotate(230deg)" ? arrowButton[i].style.transform = "rotate(45deg)" : arrowButton[i].style.transform = "rotate(230deg)";
+    }
+    bodyInfoText.style.height === 'auto' ? bodyInfoText.style.height = '90px' : bodyInfoText.style.height = 'auto';
+  } else if(document.documentElement.offsetWidth < 1440) {
+    arrowButtonLabel.textContent = arrowButtonLabel.textContent === "Читать далее" ? "Скрыть" : 'Читать далее';
+    arrowButtonLabel.style.color = 'black';
+    for(var i =0; i<arrowButton.length; i++) {
+      arrowButton[i].style.transform === "rotate(230deg)" ? arrowButton[i].style.transform = "rotate(45deg)" : arrowButton[i].style.transform = "rotate(230deg)";
+    }
+    bodyInfoText.style.height === 'auto' ? bodyInfoText.style.height = '160px' : bodyInfoText.style.height = 'auto';
+  } else{
+    arrowButtonLabel.style.color = '#666';
   }
 
 });
